53b7d5affa9b45d7939c41a28e9d1d80abada1f0
Rahil Ansari <rahilansari261@gmail.com>
2025-10-26 09:56:55 +0530

    Add 'rahil4' constant declaration in server.js for future use

--- backend/server.js
+++ backend/server.js
@@ -79,6 +79,7 @@ app.put('/api/tasks/:id', async (req, res) => {
   const rahil = 'rahil';
   const rahil2 = 'rahil2';
   const rahil3 = 'rahil3';
+  const rahil4 = 'rahil4';
 app.delete('/api/tasks/:id', async (req, res) => {
   try {
     await prisma.task.delete({
